4705b99d1a6d3cae4a7127620e09a5ac7cb23d38
leechannn <lchk0414@gmail.com>
2021-04-10 21:34:32 +0900

    pulled out crawling function

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import time
 
 
 class Post:
-    def __init__(self, post_start=1, page_type=0, page_start=1, url='', next_page='', post_xpath='', post_title_ends='', more_page_xpath='', more_page_xpath_offset='', post_limit=10, page_sub_limit=10, page_offset=2, page_offset_start=1, title='', post_mul_for_url=1):
+    def __init__(self, page_type=0, post_start=1, page_start=1, url='', next_page='', post_xpath='', more_page_xpath='', more_page_xpath_offset='', post_limit=10, page_sub_limit=10, page_offset=2, page_offset_start=1, title='', post_mul_for_url=1, special: list=None):
         self.page_type = page_type
         self.post_start = post_start
         self.page_start = page_start
@@ -17,13 +17,13 @@ class Post:
             self.more_page_xpath_offset = more_page_xpath
         else:
             self.more_page_xpath_offset = more_page_xpath_offset
-        self.post_title_ends = post_title_ends
         self.post_limit = post_limit
         self.page_sub_limit = page_sub_limit
         self.page_offset = page_offset
         self.page_offset_start = page_offset_start
         self.title = title
         self.post_mul_for_url = post_mul_for_url
+        self.special = special
 
     def get_page_url(self, num: int) -> str:
         try:
@@ -40,33 +40,28 @@ class Post:
         result = self.next_page % (num)
         return result
 
-    def get_post_title(self, title_tag: str, num: int) -> str:
-        result = (self.post_xpath % (num)) + title_tag
-        return result
-
-    def get_post_xpath(self, num: int) -> str:
-        result = (self.post_xpath % (num))
+    def get_special_xpath(self, num: int) -> str:
+        result = self.special[0] % (num)
         return result
 
 
 URLS = []
+URLS.append(Post(page_type=1,  #이거 안됨
+                 url='https://www.k-startup.go.kr/common/announcement/announcementList.do?mid=30004&bid=701&searchAppAt=A',
+                 next_page='//*[@id="btn_listAll"]',
+                 post_xpath='/html/body/div[1]/div[6]/form/div[2]/div[3]/ul[1]/li[%d]/h4/a',
+                 more_page_xpath='@@@@@here@@@@@@@@',
+                 post_limit=20))
 '''
 URLS.append(Post(page_type=0,
                  url='https://www.bizinfo.go.kr/see/seea/selectSEEA100.do',
                  next_page='?pageIndex=%d',
-                 post_xpath="""//*[@id="content"]/div[3]/div[2]/table/tbody/tr[%d]/td[2]/a""",
-                 post_title_ends='/span',
-                 post_limit=20))
-URLS.append(Post(page_type=1,  #이거 안됨
-                 url='https://www.k-startup.go.kr/common/announcement/announcementList.do?mid=30004&bid=701&searchAppAt=A',
-                 next_page="""//*[@id="btn_listAll"]""",
-                 post_xpath="""/html/body/div[1]/div[6]/form/div[2]/div[3]/ul[1]/li[%d]/h4/a""",
-                 more_page_xpath='@@@@@here@@@@@@@@',
+                 post_xpath='//*[@id="content"]/div[3]/div[2]/table/tbody/tr[%d]/td[2]/a',
                  post_limit=20))
 URLS.append(Post(page_type=0,
                  url='https://www.sdm.go.kr/news/notice/notice.do',
-                 next_page="""//*[@id="frm"]/div[2]/a[%d]""",
-                 post_xpath="""//*[@id="frm"]/table/tbody/tr[%d]/td[2]/a""",
+                 next_page='//*[@id="frm"]/div[2]/a[%d]',
+                 post_xpath='//*[@id="frm"]/table/tbody/tr[%d]/td[2]/a',
                  more_page_xpath='//*[@id="frm"]/div[2]/a[11]',
                  more_page_xpath_offset='//*[@id="frm"]/div[2]/a[13]',
                  page_offset_start=11))
@@ -89,12 +84,13 @@ URLS.append(Post(page_type=0,
                  next_page='/page/%d',
                  post_xpath='//*[@id="main-content"]/div/div[4]/div/div/div[1]/div/div[%d]/div/div[2]/h2/a',
                  post_limit=12))
-'''
 URLS.append(Post(page_type=0,  # 각 페이지 첫번째 게시물 처리 필요
                  url='https://agro.seoul.go.kr/archives/category/institution_c1/institution_news_c1/institution_news_gosi-n2',
                  next_page='/page/%d',
                  post_xpath='//*[@id="child_policyUL"]/li[%d]/a',
-                 post_limit=9))
+                 post_limit=9,
+                 special=['/html/body/div[3]/div[3]/div[2]/div[%d]/div[2]/span[1]/a', '3', '3']))
+'''
 
 
 PAGE_LIMIT = 20
@@ -106,6 +102,7 @@ def get_page_num(num: int) -> int:
     if not result:
         result = 10
     return result
+
 
 def string_escaping_for_file(string: str) -> str:
     result = string.strip()
@@ -121,6 +118,23 @@ def string_escaping_for_file(string: str) -> str:
     return result
 
 
+def crawl_post(post_start, post_limit, post_xpath, keyword):
+    global driver
+    for post_num in range(post_start, post_limit + 1):
+        title = driver.find_element_by_xpath(post_xpath % (post_num)).text
+        title = string_escaping_for_file(title)
+        print(title)
+        if title.find(keyword) != -1:  # 키워드 기반 필터링
+            # 키워드가 존재하면
+            driver.find_element_by_xpath(post_xpath % (post_num)).click()
+            time.sleep(WAIT_SEC)
+            if not os.path.isfile("./crawled/" + title + ".txt"):
+                with open("./crawled/" + title + ".txt", "w", encoding='UTF8') as f:
+                    f.write(driver.page_source)
+            driver.back()
+            time.sleep(WAIT_SEC)
+
+
 def type_page_num(URL: Post):
     global WAIT_SEC
     global driver
@@ -128,7 +142,7 @@ def type_page_num(URL: Post):
     driver.get(URL.url)
     for page_num in range(URL.page_start, PAGE_LIMIT + 1):
         next_page_num = get_page_num(page_num)
-        #print("page_num : " + str(page_num) + ", next_page_num : " + str(next_page_num))
+        # print("page_num : " + str(page_num) + ", next_page_num : " + str(next_page_num))
         if "//*" in URL.next_page:
             if URL.page_offset_start <= page_num:
                 driver.find_element_by_xpath(URL.get_page_xpath(next_page_num+URL.page_offset)).click()
@@ -137,18 +151,9 @@ def type_page_num(URL: Post):
         else:
             driver.get(URL.get_page_url(page_num))
         time.sleep(WAIT_SEC)
-        for post_num in range(URL.post_start, URL.post_limit + 1):
-            title = driver.find_element_by_xpath(URL.get_post_title(URL.post_title_ends, post_num)).text
-            title = string_escaping_for_file(title)
-            if title.find('창업') != -1:  # 키워드 기반 필터링
-                # 키워드가 존재하면
-                driver.find_element_by_xpath(URL.get_post_xpath(post_num)).click()
-                time.sleep(WAIT_SEC)
-                if not os.path.isfile("./crawled/" + title + ".txt"):
-                    with open("./crawled/" + title + ".txt", "w", encoding='UTF8') as f:
-                        f.write(driver.page_source)
-                driver.back()
-                time.sleep(WAIT_SEC)
+        if URL.special:
+            crawl_post(int(URL.special[1]), int(URL.special[2]), URL.special[0], '창업')
+        crawl_post(URL.post_start, URL.post_limit, URL.post_xpath, '창업')
         if next_page_num == 10 and "//*" in URL.next_page:
             if URL.page_offset_start <= page_num:
                 driver.find_element_by_xpath(URL.more_page_xpath_offset).click()
@@ -182,12 +187,12 @@ def type_more_post(URL: Post):
 
     for post_num in range(1, URL.post_limit + 1):
         print(post_num)
-        title = driver.find_element_by_xpath(URL.get_post_title("", post_num)).text
+        title = driver.find_element_by_xpath(URL.post_xpath % (post_num)).text
         title = string_escaping_for_file(title)
         print(title)
         if title.find('창업') != -1:  # 키워드 기반 필터링
             # 키워드가 존재하면
-            driver.find_element_by_xpath(URL.get_post_xpath(post_num)).click()
+            driver.find_element_by_xpath(URL.post_xpath % (post_num)).click()
             time.sleep(WAIT_SEC)
             if not os.path.isfile("./crawled/" + title + ".txt"):
                 with open("./crawled/" + title + ".txt", "w", encoding='UTF8') as f:
