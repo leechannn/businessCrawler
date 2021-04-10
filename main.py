ea508da85e6881470739201af523c64f737a93ef
leechannn <lchk0414@gmail.com>
2021-04-10 23:44:51 +0900

    scraping done

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import os
+import selenium
 from selenium import webdriver
 from bs4 import BeautifulSoup
 import time
@@ -46,13 +47,13 @@ class Post:
 
 
 URLS = []
+'''
 URLS.append(Post(page_type=1,  #이거 안됨
                  url='https://www.k-startup.go.kr/common/announcement/announcementList.do?mid=30004&bid=701&searchAppAt=A',
                  next_page='//*[@id="btn_listAll"]',
-                 post_xpath='/html/body/div[1]/div[6]/form/div[2]/div[3]/ul[1]/li[%d]/h4/a',
-                 more_page_xpath='@@@@@here@@@@@@@@',
-                 post_limit=20))
-'''
+                 post_xpath='/html/body/div[1]/div[6]/form/div[2]/div[3]/ul[2]/li[%d]/h4/a',
+                 more_page_xpath='//*[@id="listPlusAdd"]',
+                 special=['/html/body/div[1]/div[6]/form/div[2]/div[3]/ul[1]/li[%d]/h4/a']))
 URLS.append(Post(page_type=0,
                  url='https://www.bizinfo.go.kr/see/seea/selectSEEA100.do',
                  next_page='?pageIndex=%d',
@@ -79,12 +80,14 @@ URLS.append(Post(page_type=0,
                  url='https://sbsc.seoul.go.kr/fe/support/seoul/NR_list.do?bbsCd=1',
                  next_page='&bbsSeq=&currentPage=%d&searchVals=&bbsGrpCds_all=on&orgCd=',
                  post_xpath='//*[@id="container"]/div/div[2]/table/tbody/tr[%d]/td[2]/a'))
+'''
 URLS.append(Post(page_type=0,
                  url='https://youthhub.kr/hub/category/notice',
                  next_page='/page/%d',
                  post_xpath='//*[@id="main-content"]/div/div[4]/div/div/div[1]/div/div[%d]/div/div[2]/h2/a',
                  post_limit=12))
-URLS.append(Post(page_type=0,  # 각 페이지 첫번째 게시물 처리 필요
+'''
+URLS.append(Post(page_type=0, 
                  url='https://agro.seoul.go.kr/archives/category/institution_c1/institution_news_c1/institution_news_gosi-n2',
                  next_page='/page/%d',
                  post_xpath='//*[@id="child_policyUL"]/li[%d]/a',
@@ -118,9 +121,32 @@ def string_escaping_for_file(string: str) -> str:
     return result
 
 
-def crawl_post(post_start, post_limit, post_xpath, keyword):
+def crawl_post_page(post_start, post_limit, post_xpath, keyword):
     global driver
     for post_num in range(post_start, post_limit + 1):
+        title = driver.find_element_by_xpath(post_xpath % (post_num)).text
+        title = string_escaping_for_file(title)
+        print(title)
+        if title.find(keyword) != -1:  # 키워드 기반 필터링
+            # 키워드가 존재하면
+            driver.find_element_by_xpath(post_xpath % (post_num)).click()
+            time.sleep(WAIT_SEC)
+            if not os.path.isfile("./crawled/" + title + ".txt"):
+                with open("./crawled/" + title + ".txt", "w", encoding='UTF8') as f:
+                    f.write(driver.page_source)
+            driver.back()
+            time.sleep(WAIT_SEC)
+
+
+def crawl_post_more(post_xpath, keyword):
+    for tmp in range(1, 999):
+        try:
+            driver.find_element_by_xpath(post_xpath % (tmp))
+        except selenium.common.exceptions.NoSuchElementException as e:
+            post_limit = tmp - 1
+            break
+
+    for post_num in range(1, post_limit + 1):
         title = driver.find_element_by_xpath(post_xpath % (post_num)).text
         title = string_escaping_for_file(title)
         print(title)
@@ -152,20 +178,14 @@ def type_page_num(URL: Post):
             driver.get(URL.get_page_url(page_num))
         time.sleep(WAIT_SEC)
         if URL.special:
-            crawl_post(int(URL.special[1]), int(URL.special[2]), URL.special[0], '창업')
-        crawl_post(URL.post_start, URL.post_limit, URL.post_xpath, '창업')
+            crawl_post_page(int(URL.special[1]), int(URL.special[2]), URL.special[0], '창업')
+        crawl_post_page(URL.post_start, URL.post_limit, URL.post_xpath, '창업')
         if next_page_num == 10 and "//*" in URL.next_page:
             if URL.page_offset_start <= page_num:
                 driver.find_element_by_xpath(URL.more_page_xpath_offset).click()
             else:
                 driver.find_element_by_xpath(URL.more_page_xpath).click()
             time.sleep(WAIT_SEC)
-
-    file_list = os.listdir("./crawled")
-    for file_name in file_list:
-        with open("./crawled/" + file_name, "r", encoding='UTF8') as f:
-            html = f.read()
-        soup = BeautifulSoup(html, 'html.parser')
 
 
 def type_more_post(URL: Post):
@@ -174,38 +194,15 @@ def type_more_post(URL: Post):
 
     driver.get(URL.url)
     time.sleep(WAIT_SEC)
-    '''
-    for tmp in range(999):
+    while True:
         try:
-            driver.find_element_by_xpath(URL.get_post_xpath(tmp))
-        except selenium.common.exceptions.NoSuchElementException as e:
-            URL.post_limit = tmp - 1
+            driver.find_element_by_xpath(URL.more_page_xpath).click()
+            time.sleep(WAIT_SEC)
+        except selenium.common.exceptions.ElementNotInteractableException as e:
             break
-    '''
-    URL.post_limit = 30
-    print(URL.post_limit)
-
-    for post_num in range(1, URL.post_limit + 1):
-        print(post_num)
-        title = driver.find_element_by_xpath(URL.post_xpath % (post_num)).text
-        title = string_escaping_for_file(title)
-        print(title)
-        if title.find('창업') != -1:  # 키워드 기반 필터링
-            # 키워드가 존재하면
-            driver.find_element_by_xpath(URL.post_xpath % (post_num)).click()
-            time.sleep(WAIT_SEC)
-            if not os.path.isfile("./crawled/" + title + ".txt"):
-                with open("./crawled/" + title + ".txt", "w", encoding='UTF8') as f:
-                    f.write(driver.page_source)
-            driver.back()
-            time.sleep(WAIT_SEC)
-'''
-    file_list = os.listdir("./crawled")
-    for file_name in file_list:
-        with open("./crawled/" + file_name, "r", encoding='UTF8') as f:
-            html = f.read()
-        soup = BeautifulSoup(html, 'html.parser')
-'''
+    if URL.special:
+        crawl_post_more(URL.special[0], '창업')
+    crawl_post_more(URL.post_xpath, '창업')
 
 
 def type_one_page(URL: Post):
@@ -230,3 +227,10 @@ if __name__ == '__main__':
         elif URL.page_type == 2:
             type_one_page(URL)
 
+'''
+    file_list = os.listdir("./crawled")
+    for file_name in file_list:
+        with open("./crawled/" + file_name, "r", encoding='UTF8') as f:
+            html = f.read()
+        soup = BeautifulSoup(html, 'html.parser')
+'''
